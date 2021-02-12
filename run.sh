1f6a24c5d1119280d1d8d7b4f0d8eba2b1e3cf9b
AndrewGibson27 <atgibson27@gmail.com>
2021-02-11 19:27:54 -0600

    pretty colors

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CYAN='\033[0;36m'
+NO_COLOR='\033[0m'
+
 # Process CLI arguments
 while getopts ":f:s:n:" opt; do
   case $opt in
@@ -14,14 +17,14 @@ rm -rf reports/
 
 # Run Lighthouse
 for ((run = 1; run <= $numRuns; run++)); do
-  echo "Starting Lighthouse run $run on $firstUrl ..."
+  echo -e "${CYAN}Starting Lighthouse run $run on $firstUrl...${NO_COLOR}"
   docker run -it --rm \
     --volume=/$(pwd)/reports/first-url:/home/chrome/reports/ \
     --name=lighthouse \
     --cap-add=SYS_ADMIN \
     femtopixel/google-lighthouse $firstUrl --quiet --only-categories=performance --skip-audits=full-page-screenshot --output html,json --output-path=run-$run
 
-  echo "Starting Lighthouse run $run on $secondUrl ..."
+  echo -e "${CYAN}Starting Lighthouse run $run on $secondUrl...${NO_COLOR}"
   docker run -it --rm \
     --volume=/$(pwd)/reports/second-url:/home/chrome/reports/ \
     --name=lighthouse \
@@ -30,8 +33,10 @@ for ((run = 1; run <= $numRuns; run++)); do
 done
 
 # Find the median run for each URL
-echo "Finding the median runs ..."
+echo -e "${CYAN}Finding the median runs...${NO_COLOR}"
 docker build -f Dockerfile.median -t lh-median .
 docker run --rm \
   --volume=/$(pwd):/app \
   --name=lh-median lh-median
+
+echo -e "${CYAN}Done!${NO_COLOR}"
